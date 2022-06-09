ebbb8ec0799a93e652db5ab4fea44322f3d89fc3
danganhphu <phudanganh107@gmail.com>
2022-06-09 10:34:18 +0700

    // update cmt dễ hiểu

--- QuanLyNhanVien/HopDongLaoDongHandler.cs
+++ QuanLyNhanVien/HopDongLaoDongHandler.cs
@@ -31,32 +31,29 @@ namespace QuanLyNhanVien
 
             company = doc.CreateElement("congty");
 
-            foreach (HopDongLaoDong hd in hopDongList)//Duyet het cac nhan vien co trong danh sach
+            foreach (HopDongLaoDong hd in hopDongList)
             {
-                hopDong = doc.CreateElement("HopDongLD"); //tao moi element nhanvien
+                hopDong = doc.CreateElement("HopDongLD");
                 hdId = doc.CreateAttribute("MaHD");
                 hdId.Value = hd.HdId;
-                empId = doc.CreateAttribute("manv"); //tao moi thuoc tinh manv
-                empId.Value = hd.EmpId; //gan gia tri cho thuoc tinh mã nv
+                empId = doc.CreateAttribute("manv");
+                empId.Value = hd.EmpId;
                 hopDong.Attributes.Append(hdId);
-                hopDong.Attributes.Append(empId); //them thuoc tinh manv vao nv
+                hopDong.Attributes.Append(empId);
 
-                hdName = doc.CreateElement("TenHD"); //tao moi element hoten
-                hdName.InnerText = hd.HdName; //gan gia tri cho element hoten
+                hdName = doc.CreateElement("TenHD");
+                hdName.InnerText = hd.HdName;
 
-                ngayBD = doc.CreateElement("NgayBD"); //tao moi element luong
-                ngayBD.InnerText = hd.NgayBD; //gan gia tri cho element luong
-                //vi luong nv la float nen can chuyen sang chuoi ta dung float.toString()
+                ngayBD = doc.CreateElement("NgayBD");
+                ngayBD.InnerText = hd.NgayBD;
 
-                ngayKT = doc.CreateElement("NgayKT"); //tao moi element luong
+                ngayKT = doc.CreateElement("NgayKT");
                 ngayKT.InnerText = hd.NgayKT;
 
-                hopDong.AppendChild(hdName); //them element hoten vao element nhanvien
-                hopDong.AppendChild(ngayBD); //them element luong vao element nhanvien
-                hopDong.AppendChild(ngayKT); //them element phongban vao element nhanvien
-
-                company.AppendChild(hopDong); /*them element nhanvien vao element congty
-                                                element congty chinh la nut goc(root) */
+                hopDong.AppendChild(hdName);
+                hopDong.AppendChild(ngayBD);
+                hopDong.AppendChild(ngayKT);
+                company.AppendChild(hopDong);
             }
             doc.AppendChild(company); //them element congty vao tai lieu
 
@@ -67,40 +64,37 @@ namespace QuanLyNhanVien
         {
             doc = new XmlDocument(); //Tao moi 1 doi tuong XmlDocument
             doc.Load(filename); //Load file xml theo duong dan
-            hopDongList = new List<HopDongLaoDong>(); /*Tao moi 1 danh sach nhan vien de
-                                            luu tru cac nhan vien */
+            hopDongList = new List<HopDongLaoDong>();
             loadDataFromDoc(doc, filename, hopDongList);
             if (isExistId(hopDongList, hd.HdId)) //Kiem tra da ton tai ma
                 return false; //da ton tai tra ve false
-            //Khong roi vao hai truong hop day ta di thuc hien them nhan vien
+            //Khong roi vao hai truong hop day ta di thuc hien them hop dong
 
             XmlElement hopDong, hdName, ngayBD, ngayKT;
             XmlAttribute hdId, empId;
 
-            hopDong = doc.CreateElement("HopDongLD"); //tao moi element nhanvien
+            hopDong = doc.CreateElement("HopDongLD");
             hdId = doc.CreateAttribute("MaHD");
             hdId.Value = hd.HdId;
-            empId = doc.CreateAttribute("manv"); //tao moi thuoc tinh manv
-            empId.Value = hd.EmpId; //gan gia tri cho thuoc tinh manv
+            empId = doc.CreateAttribute("manv");
+            empId.Value = hd.EmpId;
             hopDong.Attributes.Append(hdId);
-            hopDong.Attributes.Append(empId); //them thuoc tinh manv vao nv
+            hopDong.Attributes.Append(empId);
 
-            hdName = doc.CreateElement("TenHD"); //tao moi element hoten
-            hdName.InnerText = hd.HdName; //gan gia tri cho element hoten
+            hdName = doc.CreateElement("TenHD");
+            hdName.InnerText = hd.HdName;
 
-            ngayBD = doc.CreateElement("NgayBD"); //tao moi element luong
-            ngayBD.InnerText = hd.NgayBD.ToString(); //gan gia tri cho element luong
-                                                     //vi luong nv la float nen can chuyen sang chuoi ta dung float.toString()
+            ngayBD = doc.CreateElement("NgayBD");
+            ngayBD.InnerText = hd.NgayBD.ToString();
 
-            ngayKT = doc.CreateElement("NgayKT"); //tao moi element luong
+            ngayKT = doc.CreateElement("NgayKT");
             ngayKT.InnerText = hd.NgayKT.ToString();
 
-            hopDong.AppendChild(hdName); //them element hoten vao element nhanvien
-            hopDong.AppendChild(ngayBD); //them element luong vao element nhanvien
-            hopDong.AppendChild(ngayKT); //them element phongban vao element nhanvien
+            hopDong.AppendChild(hdName);
+            hopDong.AppendChild(ngayBD);
+            hopDong.AppendChild(ngayKT);
 
-            doc.DocumentElement.AppendChild(hopDong); //them nhan vien vao nut goc tai lieu
-            //DocumneTelement la nut goc cua tai lieu
+            doc.DocumentElement.AppendChild(hopDong);
 
             doc.Save(filename);
 
@@ -109,23 +103,20 @@ namespace QuanLyNhanVien
 
         public bool edit(HopDongLaoDong hd)
         {
-            doc = new XmlDocument(); //Tao moi 1 doi tuong XmlDocument
-            doc.Load(filename); //Load file xml theo duong dan
-            hopDongList = new List<HopDongLaoDong>(); /*Tao moi 1 danh sach nhan vien de
-                                            luu tru cac nhan vien */
-            loadDataFromDoc(doc, filename, hopDongList); /*Lay cac nhan vien tu tai lieu roi
-                                                     them vao danh sach nhan vien vua tao */
+            doc = new XmlDocument();
+            doc.Load(filename);
+            hopDongList = new List<HopDongLaoDong>();
+            loadDataFromDoc(doc, filename, hopDongList);
             if (!isExistId(hopDongList, hd.HdId)) //Kiem tra ma co ton tai hay khong
                 return false; //Neu khong tra ve false
 
             XmlNodeList empNode = doc.GetElementsByTagName("HopDongLD");
-            //Lay danh sach cac nut nahn vien tu tai lieu
 
             foreach (XmlNode node in empNode)
             {
                 string hdId = node.Attributes["MaHD"].Value;
-                //Lay ra manv cua nut nhan vien hien tai
-                if (hdId == hd.HdId) //Neu ma nv cua nut hien tai trung voi ma nv can sua
+
+                if (hdId == hd.HdId)
                 {
                     //Thuc hien sua doi thong tin cua nut nay
                     node.Attributes["manv"].Value = hd.EmpId;
@@ -145,21 +136,18 @@ namespace QuanLyNhanVien
         {
             doc = new XmlDocument(); //Tao moi 1 doi tuong XmlDocument
             doc.Load(filename); //Load file xml theo duong dan
-            hopDongList = new List<HopDongLaoDong>(); /*Tao moi 1 danh sach nhan vien de
-                                            luu tru cac nhan vien */
-            loadDataFromDoc(doc, filename, hopDongList); /*Lay cac nhan vien tu tai lieu roi
-                                                     them vao danh sach nhan vien vua tao */
+            hopDongList = new List<HopDongLaoDong>();
+            loadDataFromDoc(doc, filename, hopDongList);
             if (!isExistId(hopDongList, hdId)) //Kiem tra ma co ton tai hay khong
                 return false; //Neu khong tra ve false
 
             XmlNodeList empNode = doc.GetElementsByTagName("HopDongLD");
-            //Lay danh sach cac nut nahn vien tu tai lieu
 
             foreach (XmlNode node in empNode) //Duyet danh sach cac nut nhan vien
             {
                 string id = node.Attributes["MaHD"].Value;
-                //Lay ra manv cua nut nhan vien hien tai
-                if (id == hdId) //Neu ma nv cua nut hien tai trung voi ma nv can sua
+
+                if (id == hdId)
                 {
                     doc.DocumentElement.RemoveChild(node);
                     break;
@@ -179,24 +167,23 @@ namespace QuanLyNhanVien
             //Lay ra cac nut co the ten la nhanvien
             foreach (XmlNode node in empNode) //Duyet tung nut
             {
-                //Khai bao cac bien luu tri thong tin cua nhan vien
-                string hdId; //ma hdong
-                string empId; //ma nhan vien
-                string hdName; //ten nhan vien
-                string ngayBD; //luong
-                string ngayKT; //ten phong ban
+                string hdId;
+                string empId;
+                string hdName;
+                string ngayBD;
+                string ngayKT;
 
                 hdId = node.Attributes["MaHD"].Value; //Lay thuoc tinh manv
                 empId = node.Attributes["manv"].Value;
 
                 hdName = node.ChildNodes[0].InnerText;
-                //Lay ra gia tri nut con cua nut nhanvien co index = 0
+
                 ngayBD = node.ChildNodes[1].InnerText;
-                //Lay ra gia tri nut con cua nut nhanvien co index = 1
+
                 ngayKT = node.ChildNodes[2].InnerText;
 
                 HopDongLaoDong hd = new HopDongLaoDong(hdId, empId, hdName, ngayBD, ngayKT);
-                //Tao moi mot nhan vien co nhung gia tri vua lay dc
+
                 hopDongList.Add(hd);
             }
         }
@@ -205,9 +192,9 @@ namespace QuanLyNhanVien
         {
             bool isExist = false;//ban dau se cho la chua trung ma
 
-            foreach (HopDongLaoDong hd in hopDongList)//Duyet tung nhan vien trong danh sach
+            foreach (HopDongLaoDong hd in hopDongList)//Duyet tung hop dong trong danh sach
             {
-                string hdId = hd.HdId;//lay ra id cua nhan vien hien tai
+                string hdId = hd.HdId;//lay ra id  hien tai
                 if (hdId.ToLower() == id.ToLower()) //chuyen sang chu thuong va so sanh
                 {
                     //neu ma hai chuoi bang nhau
